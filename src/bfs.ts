233d23854e1e8eb1d54f3dfdb0e13140a07fa61a
Jared Johnson <jared@jaredjohnson.dev>
2022-11-06 12:32:52 -0500

    Simplify TreeNode bfs by removing seen tracking

--- src/bfs.ts
+++ src/bfs.ts
@@ -5,21 +5,15 @@ export type Predicate<T> = (node: TreeNode<T>) => boolean;
 
 export const bfs = <T>(root: TreeNode<T>, predicate: Predicate<T>): TreeNode<T> | null => {
   const queue = new Queue<TreeNode<T>>();
-  const seen = new Set<TreeNode<T>>();
 
   queue.enqueue(root);
   while (!queue.isEmpty()) {
     const node = queue.dequeue()!;
-    seen.add(node);
-
     if (predicate(node)) {
       return node;
     }
-
     for (const child of node.children) {
-      if (!seen.has(child)) {
-        queue.enqueue(child);
-      }
+      queue.enqueue(child);
     }
   }
 
