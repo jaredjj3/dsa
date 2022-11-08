c9effd20e757e14489733e3769c072bf2c0a1904
Jared Johnson <jared@jaredjohnson.dev>
2022-11-08 06:21:19 -0500

    Enhance BTreeNode by adding children API

--- src/BTreeNode.ts
+++ src/BTreeNode.ts
@@ -4,4 +4,15 @@ export class BTreeNode<T> {
     public left: BTreeNode<T> | null = null,
     public right: BTreeNode<T> | null = null
   ) {}
+
+  get children(): BTreeNode<T>[] {
+    const children = new Array<BTreeNode<T>>();
+    if (this.left) {
+      children.push(this.left);
+    }
+    if (this.right) {
+      children.push(this.right);
+    }
+    return children;
+  }
 }
