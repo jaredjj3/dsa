aa05e4534595b29111f5f475f5bcd6d8fb1495d9
Jared Johnson <jared@jaredjohnson.dev>
2022-11-08 20:04:31 -0500

    Implement and test recursive combinations

--- src/combinations.test.ts
+++ src/combinations.test.ts
@@ -1,15 +1,24 @@
 import { combinations } from './combinations';
 
 describe('combinations', () => {
-  describe.each([
-    { name: 'default', combinations: combinations.default },
-    { name: 'iterative', combinations: combinations.iterative },
-    { name: 'recursive', combinations: combinations.recursive },
-  ])(`combinations variant: '$name'`, (spec) => {
-    const combinations = spec.combinations;
+  describe.each([{ name: 'recursive', combinations: combinations.recursive }])(
+    `combinations variant: '$name'`,
+    (spec) => {
+      const combinations = spec.combinations;
 
-    it.todo('calculates all combinations of an array of items');
+      it.each([
+        { items: [1], expected: [[], [1]] },
+        { items: [1, 2], expected: [[], [1], [2], [1, 2]] },
+        { items: [1, 2, 3], expected: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]] },
+      ])('calculates all combinations of an array of items: $items', (t) => {
+        const combos = combinations(t.items);
+        expect(combos).toStrictEqual(t.expected);
+      });
 
-    it.todo('calculates the combinations of an empty array');
-  });
+      it('calculates the combinations of an empty array', () => {
+        const combos = combinations([]);
+        expect(combos).toStrictEqual([[]]);
+      });
+    }
+  );
 });
--- src/combinations.ts
+++ src/combinations.ts
@@ -1,7 +1,12 @@
 export type CombinationsGenerator<T> = (items: T[]) => T[];
 
-const recursive = <T>(items: T[]): T[] => {
-  return [];
+const recursive = <T>(items: T[]): T[][] => {
+  if (items.length === 0) {
+    return [[]];
+  }
+  const subs = recursive(items.slice(0, -1));
+  const last = items[items.length - 1];
+  return [...subs, ...subs.map((sub) => [...sub, last])];
 };
 
 const iterative = <T>(items: T[]): T[] => {
