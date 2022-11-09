2cfa32a846c741258d1dc1b8f1d7046232e36868
Jared Johnson <jared@jaredjohnson.dev>
2022-11-08 19:46:56 -0500

    Test getChildren for BTreeNode and TreeNode

--- src/BTreeNode.test.ts
+++ src/BTreeNode.test.ts
@@ -1,7 +1,37 @@
 import { BTreeNode } from './BTreeNode';
 
 describe(BTreeNode, () => {
-  it('instantiates without crashing', () => {
-    expect(() => new BTreeNode(0)).not.toThrow();
+  describe('getChildren', () => {
+    it('returns the left and right child when present', () => {
+      const root = new BTreeNode(0);
+      const left = new BTreeNode(1);
+      const right = new BTreeNode(2);
+      root.left = left;
+      root.right = right;
+
+      expect(root.getChildren()).toStrictEqual([left, right]);
+    });
+
+    it('omits the left child when null', () => {
+      const root = new BTreeNode(0);
+      const right = new BTreeNode(2);
+      root.right = right;
+
+      expect(root.getChildren()).toStrictEqual([right]);
+    });
+
+    it('omits the right child when null', () => {
+      const root = new BTreeNode(0);
+      const left = new BTreeNode(1);
+      root.left = left;
+
+      expect(root.getChildren()).toStrictEqual([left]);
+    });
+
+    it('returns an empty array when no child', () => {
+      const root = new BTreeNode(0);
+
+      expect(root.getChildren()).toStrictEqual([]);
+    });
   });
 });
--- src/TreeNode.test.ts
+++ src/TreeNode.test.ts
@@ -1,7 +1,14 @@
 import { TreeNode } from './TreeNode';
 
 describe(TreeNode, () => {
-  it('instantiates without crashing', () => {
-    expect(() => new TreeNode(1)).not.toThrow();
+  describe('getChildren', () => {
+    it('returns the children of the node', () => {
+      const root = new TreeNode(0);
+      const node1 = new TreeNode(1);
+      const node2 = new TreeNode(2);
+      root.children = [node1, node2];
+
+      expect(root.getChildren()).toStrictEqual([node1, node2]);
+    });
   });
 });
