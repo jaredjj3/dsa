ea13d301493783db7d978cfb31fa5128b1423bfc
Jared Johnson <jared@jaredjohnson.dev>
2022-11-08 20:12:55 -0500

    Install jest-extended and improve tests

--- src/combinations.test.ts
+++ src/combinations.test.ts
@@ -12,12 +12,12 @@ describe('combinations', () => {
         { items: [1, 2, 3], expected: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]] },
       ])('calculates all combinations of an array of items: $items', (t) => {
         const combos = combinations(t.items);
-        expect(combos).toStrictEqual(t.expected);
+        expect(combos).toIncludeAllMembers(t.expected);
       });
 
       it('calculates the combinations of an empty array', () => {
         const combos = combinations([]);
-        expect(combos).toStrictEqual([[]]);
+        expect(combos).toIncludeAllMembers([[]]);
       });
     }
   );
