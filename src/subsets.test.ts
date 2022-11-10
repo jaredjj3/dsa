96d08fae8d87faea8095ad69cb71bcfe21dd8267
Jared Johnson <jared@jaredjohnson.dev>
2022-11-10 08:41:40 -0500

    Rename combinations to subsets

--- src/subsets.test.ts
+++ src/subsets.test.ts
@@ -1,15 +1,21 @@
 import { subsets } from './subsets';
 
 describe('subsets', () => {
-  describe.each([
-    { name: 'default', subsets: subsets.default },
-    { name: 'iterative', subsets: subsets.iterative },
-    { name: 'recursive', subsets: subsets.recursive },
-  ])(`subsets variant: '$name'`, (spec) => {
+  describe.each([{ name: 'recursive', subsets: subsets.recursive }])(`subsets variant: '$name'`, (spec) => {
     const subsets = spec.subsets;
 
-    it.todo('calculates all subsets of an array of items');
+    it.each([
+      { items: [1], expected: [[], [1]] },
+      { items: [1, 2], expected: [[], [1], [2], [1, 2]] },
+      { items: [1, 2, 3], expected: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]] },
+    ])('calculates all subsets of an array of items: $items', (t) => {
+      const combos = subsets(t.items);
+      expect(combos).toIncludeAllMembers(t.expected);
+    });
 
-    it.todo('calculates the subsets of an empty array');
+    it('calculates the subsets of an empty array', () => {
+      const combos = subsets([]);
+      expect(combos).toIncludeAllMembers([[]]);
+    });
   });
 });
--- src/subsets.ts
+++ src/subsets.ts
@@ -1,15 +1,15 @@
 export type SubsetsGenerator<T> = (items: T[]) => T[];
 
-const recursive = <T>(items: T[]): T[] => {
-  return [];
-};
-
-const iterative = <T>(items: T[]): T[] => {
-  return [];
+const recursive = <T>(items: T[]): T[][] => {
+  if (items.length === 0) {
+    return [[]];
+  }
+  const subs = recursive(items.slice(0, -1));
+  const last = items[items.length - 1];
+  return [...subs, ...subs.map((sub) => [...sub, last])];
 };
 
 export const subsets = {
-  default: iterative,
-  iterative,
+  default: recursive,
   recursive,
 };
