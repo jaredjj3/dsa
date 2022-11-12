ebc58956c980aa6536ca74b3a7b4b6ebe65c06c5
Jared Johnson <jared@jaredjohnson.dev>
2022-11-12 11:35:00 -0500

    Simplify ListNode documentation

--- src/ListNode.ts
+++ src/ListNode.ts
@@ -1,9 +1,5 @@
 /**
  * A doubly-linked list node.
- *
- * This data structure is used to store sequences of data. Its main advantage over an array is that it supports adding
- * and removing nodes in O(1) time. However, coordinating pointers usually results in slightly less maintainable code,
- * and callers must have a reference to a node in order to perform a mutation.
  */
 export class ListNode<T> {
   private next: ListNode<T> | null = null;
