7d24e0e420253d70067321c7f67931b36d97e949
Jared Johnson <jared@jaredjohnson.dev>
2022-11-12 11:27:55 -0500

    Document permutations

--- src/permutations.ts
+++ src/permutations.ts
@@ -8,6 +8,9 @@ const iterative = <T>(items: T[]): T[] => {
   return [];
 };
 
+/**
+ * Calculates all permutations of an object.
+ */
 export const permutations = {
   default: iterative,
   iterative,
